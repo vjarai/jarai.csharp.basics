2c16bc5ad82f6ea9d86f6869b8557a341eccb747
Viktor <vjarai@users.noreply.github.com>
2022-10-04 12:23:56 +0200

    Umstellung Basics auf Net Core + Nullable Types

---
+++ Basics10.NullableTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Basics10.NullableTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nullable Variablen werden mit ? deklariert
+            int? x = null;
+            
+            // Abfrage, ob der Wert von x gesetzt wurde
+            if(!x.HasValue)
+                Console.WriteLine("Der Wert von x ist unbekannt (null)!");
+
+            // Der Coalesce operator (??) übernimmt den rechten Wert als Default, wenn die linke Seite null ist
+            var y = x ?? 100;
+
+            Console.WriteLine($"Wert von y ist jetzt: {y}");
+
+            x = 1;
+
+            Console.WriteLine($"Wert von x ist jetzt: {x}");
+
+            Console.ReadLine();
+        }
+    }
+}
