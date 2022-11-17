0ca18823e5cc04efba079307ff7ebaacc59eb68b
Viktor <viktor@nospam.org>
2022-11-17 10:36:02 +0100

    .

--- Basics10.NullableTypes/Program.cs
+++ Basics10.NullableTypes/Program.cs
@@ -4,21 +4,25 @@
     {
         static void Main(string[] args)
         {
-            // Nullable Variablen werden mit ? deklariert
-            int? x = null;
-            
-            // Abfrage, ob der Wert von x gesetzt wurde
-            if(!x.HasValue)
-                Console.WriteLine("Der Wert von x ist unbekannt (null)!");
+            // Nullable<bool> esRegnet;
+
+            // Oder einfacher mit ?
+            bool? esRegnet;
+
+            esRegnet = null;
+            esRegnet = true;
+            esRegnet = false;
+
+            // Abfrage, ob der Wert von esRegnet gesetzt ist
+            if (!esRegnet.HasValue)
+                Console.WriteLine("Ich weis nicht, ob es regnet.");
+            else
+                Console.WriteLine($"Wert von esRegnet ist: {esRegnet}");
 
             // Der Coalesce operator (??) übernimmt den rechten Wert als Default, wenn die linke Seite null ist
-            var y = x ?? 100;
+            esRegnet = esRegnet ?? true;
 
-            Console.WriteLine($"Wert von y ist jetzt: {y}");
-
-            x = 1;
-
-            Console.WriteLine($"Wert von x ist jetzt: {x}");
+            Console.WriteLine($"Wert von x: {esRegnet}");
 
             Console.ReadLine();
         }
