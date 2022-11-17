1e3bf6e8011c3fb93cc4e3ecda58037bcb975a66
Viktor <viktor@nospam.org>
2022-11-17 07:41:04 +0100

    .

---
+++ Calculator.UserInterface.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Jarai.CSharp.Calculator.Logic;
+
+namespace Jarai.CSharp.Calculator.UserInterface.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Erste Zahl?");
+            string eingabe = Console.ReadLine();
+            var zahl1 = double.Parse(eingabe);
+
+            Console.WriteLine("Zweite Zahl?");
+            eingabe = Console.ReadLine();
+            var zahl2 = double.Parse(eingabe);
+
+            var calculator = new CalculationService();
+            double ergebnis = calculator.Add(zahl1, zahl2);
+
+            Console.WriteLine($"Ergebnis: {zahl1} + {zahl2} = {ergebnis}");
+
+        }
+    }
+}
