d28b3d13a3fb5c4301546310a0a11bfd0f9cbdf8
Viktor <viktor@nospam.org>
2023-03-29 09:45:02 +0200

    .

--- Basics6.Exceptions/ReadMe/Basics06_Exceptions.md
+++ Basics6.Exceptions/ReadMe/Basics06_Exceptions.md
@@ -2,8 +2,8 @@
 
 ### Erstellen Sie das Projekt "Partyrechner":
 
-- Fragen Sie den Anwender wieviele Personen zur Party kommen und wieviele Kuchen vorhanden sind.
-- Berchnen Sie als ergebnis wieviele Kuchen pro Person zur Verfügung stehen.
+- Fragen Sie den Anwender, wieviele Personen zur Party kommen und wieviele Kuchen vorhanden sind.
+- Berechnen Sie als ergebnis, wieviele Kuchen pro Person zur Verfügung stehen.
 - Fangen Sie dabei mögliche Laufzeitfehler bei nichtnumerischen Eingaben ab.
 - Bei 0 Gästen oder 0 Kuchen wird Main mit throw new Exception("Keine Party") abgebrochen.
 
---
+++ Objektorientierung4.Konto/Konto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Objektorientierung4.Konto
+{
+    public class Konto
+    {
+        private int _kontoNummer;
+        private decimal _kontoStand;
+        private string _kontoInhaber;
+        private static int _anzahlKonten;
+        private int _pin;
+
+        public Konto(string kontoInhaber)
+        {
+            _kontoInhaber = kontoInhaber;
+            _pin = 0;
+            _kontoNummer = ++_anzahlKonten;
+        }
+
+        public void Anzeigen()
+        {
+            Console.WriteLine($"Konto: {_kontoNummer}");
+        }
+
+        public void Einzahlen(decimal betrag)
+        {
+            _kontoStand += betrag;
+
+            Console.WriteLine($"{betrag} EUR auf Konto {_kontoNummer} eingezahlt. " +
+                              $"Neuer Kontostand: {_kontoStand}");
+        }
+
+        public void Abheben(decimal betrag, int pin)
+        {
+            if (pin != _pin)
+                throw new Exception("Falsche Pin!");
+
+            if (betrag > _kontoStand)
+                throw new Exception("Keine Deckung!");
+
+            _kontoStand -= betrag;
+
+            Console.WriteLine($"{betrag} EUR von Konto {_kontoNummer} abgehoben. " +
+                              $"Neuer Kontostand: {_kontoStand}");
+
+        }
+
+        public void Überweisen(Konto habenkonto, int pin, decimal betrag)
+        {
+        }
+    }
+}
