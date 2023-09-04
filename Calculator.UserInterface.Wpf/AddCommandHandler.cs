cb7330ae9ff32f58162809ea05277d7e5c957a4e
Viktor <viktor@nospam.org>
2023-09-04 07:36:36 +0200

    .

---
+++ Calculator.UserInterface.Wpf/AddCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jarai.CSharp.Calculator.UserInterface.Wpf
+{
+    public class AddCommandHandler : System.Windows.Input.ICommand
+    {
+        public ViewModels.MainWindowViewModel MainWindowViewModel
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
