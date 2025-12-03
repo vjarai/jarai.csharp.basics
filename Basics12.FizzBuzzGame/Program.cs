f9ad8847e3b0571e4b071f45e4cfacdf9652119e
Viktor <vjarai@users.noreply.github.com>
2025-12-03 08:19:39 +0100

    .

---
+++ Basics12.FizzBuzzGame/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Basics12.FizzBuzzGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FizzBuzz Game (1-100):");
+            Console.WriteLine();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                string result = FizzBuzz(i);
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("FizzBuzz Variation (with digit check):");
+            Console.WriteLine();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                string result = FizzBuzzWithDigitCheck(i);
+                Console.WriteLine(result);
+            }
+        }
+
+        static string FizzBuzz(int zahl)
+        {
+            if (zahl % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (zahl % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else if (zahl % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return zahl.ToString();
+            }
+        }
+
+
+
+        static string FizzBuzzWithDigitCheck(int zahl)
+        {
+            bool isFizz = zahl % 3 == 0 || zahl.ToString().Contains('3');
+            bool isBuzz = zahl % 5 == 0 || zahl.ToString().Contains('5');
+
+            if (isFizz && isBuzz)
+            {
+                return "FizzBuzz";
+            }
+            else if (isFizz)
+            {
+                return "Fizz";
+            }
+            else if (isBuzz)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return zahl.ToString();
+            }
+        }
+    }
+}
+
---
+++ Basics12.FizzBuzzGame/_ReadMe/Aufgabe.md
@@ -0,0 +1,25 @@
+﻿# Übung „FizzBuzz“ Spiel
+
+Schreibe eine Funktion, die die Zahlen von 1 bis 100 zurückgibt. Manche Zahlen sollen dabei allerdings übersetzt werden:
+-	Für Vielfache von 3 gib „Fizz“ aus.
+- 	Für Vielfache von 5 gib „Buzz“ aus.
+-	Für Vielfache von 3 und 5 gib „FizzBuzz“ aus.
+  - 
+Beispiel:
+```
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+...
+14
+FizzBuz
+16
+...
+```
+## Variation
+„Fizz“ soll auch zurückgegeben werden, wenn die Zahl eine 3 als Ziffer enthält, z.B. 13. „Buzz“ soll auch zurückgegeben werden, wenn die Zahl eine 5 als Ziffer enthält, z.B. 52.
+
